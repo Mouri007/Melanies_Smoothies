047c24df5c947adccc2292d1e5219636e1dc6037
Mouri007 <163134082+Mouri007@users.noreply.github.com>
2024-04-04 17:19:30 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,7 @@ if ing_list:
                fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + friut_chosen)
                fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
              
+  
     my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
             values ('""" + ing_string + """','"""+ name_on_order+"""')"""
 
